d58052162987d0597fe400969aae29defd5cc8b5
Daniel Galindo <josedaniel.galindo@hotmail.com>
2022-08-01 18:59:43 -0500

    Set up webpack enviroment

--- webpack.config.js
+++ webpack.config.js
@@ -13,7 +13,7 @@ module.exports = {
     }),
   ],
   output: {
-    filename: '[name].bundle.js',
+    filename: '[name]-[hash].js',
     path: path.resolve(__dirname, 'dist'),
     clean: true,
   },

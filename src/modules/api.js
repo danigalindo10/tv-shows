34d5eaf70a0fd5ac44a081867ef668d62cbb4335
Daniel Galindo <josedaniel.galindo@hotmail.com>
2022-08-01 22:27:14 -0500

    Display list of items on the Homepage
    Add Styles

---
+++ src/modules/api.js
@@ -0,0 +1,6 @@
+const tvApi = 'https://api.tvmaze.com';
+
+const baseUrl = 'https://us-central1-involvement-api.cloudfunctions.net/capstoneApi/apps';
+const appId = 'JbH05JvjcbTpE6SAjZIh';
+
+export { tvApi, appId, baseUrl };

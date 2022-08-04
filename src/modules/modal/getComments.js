98e9c6c4eff464dc1bbb1dd71e28a1d0504ceeb5
Daniel Galindo <josedaniel.galindo@hotmail.com>
2022-08-04 10:22:50 -0500

    Fix linter errors

--- src/modules/modal/getComments.js
+++ src/modules/modal/getComments.js
@@ -1,18 +1,17 @@
 import { appId, baseUrl } from '../api';
 
 const getComments = async (id) => {
-
-    const response = await fetch(`${baseUrl}/${appId}/comments?item_id=${id}`, {
-        method: 'GET',
-        headers: {
-            Accept: 'application/json'
-        },
-    });
-    if (response.status === 500) {
-        return response.json();
-    }
-    const jsonResponse = await response.json();
-    return Promise.reject(jsonResponse)
+  const response = await fetch(`${baseUrl}/${appId}/comments?item_id=${id}`, {
+    method: 'GET',
+    headers: {
+      Accept: 'application/json',
+    },
+  });
+  if (response.status === 500) {
+    return response.json();
+  }
+  const jsonResponse = await response.json();
+  return Promise.reject(jsonResponse);
 };
 
 export default getComments;

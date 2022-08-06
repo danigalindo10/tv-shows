2da7335f9fa97bb99b238922ec20a5c5555fb160
Daniel Galindo <josedaniel.galindo@hotmail.com>
2022-08-06 08:18:55 -0500

    Modify Style

--- src/modules/modal/populateComments.js
+++ src/modules/modal/populateComments.js
@@ -4,11 +4,9 @@ const createComments = async (Comment, commentArea) => {
   const userDate = document.createElement('p');
   userDate.textContent = Comment.creation_date;
   individualComment.appendChild(userDate);
-  // commentArea.appendChild(individualComment);
   const userName = document.createElement('p');
   userName.textContent = Comment.username;
   individualComment.appendChild(userName);
-  // commentArea.appendChild(individualComment);
   const userComment = document.createElement('p');
   userComment.textContent = Comment.comment;
   individualComment.appendChild(userComment);

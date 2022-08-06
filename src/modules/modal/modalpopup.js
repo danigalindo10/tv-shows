ebe5b2cbebf32c3c385eaa933b5ab7af8c863182
Eternalgratis <tyaps002@gmail.com>
2022-08-06 02:31:30 +0100

    Fix linters

--- src/modules/modal/modalpopup.js
+++ src/modules/modal/modalpopup.js
@@ -1,7 +1,4 @@
-/* eslint-disable */
 import getComments from './getComments';
-import postComments from './postcomments';
-import getCommentsCounter from './commentCounter';
 import createComments from './populateComments';
 
 const Modal = async (dataname, dataimagemedium, dataid, datasummary) => {
@@ -37,20 +34,25 @@ const Modal = async (dataname, dataimagemedium, dataid, datasummary) => {
   const comments = document.createElement('div');
   comments.classList.add('comments-popup');
 
-  const commentsCard = document.createElement('button');
+  const commentsCard = document.createElement('div');
   commentsCard.classList.add('comments');
+  comments.appendChild(commentsCard);
 
-  const commentUpdate = async () => {
+  const commentArea = document.createElement('div');
+  commentArea.classList.add('comment-area');
+  commentsCard.appendChild(commentArea);
+
+  const commentUpdate = async (commentArea) => {
     const comm = await getComments(dataid).catch(() => []);
-    if (comm.length) {
-      createComments(comm, formContainer);
-      const commentsCount = getCommentsCounter();
-      comments.textContent = `comments (${commentsCount})`;
-    } else {
+    if (comm.length < 1 || comm.length === undefined) {
       comments.textContent = 'comments (0)';
+    } else {
+      comm.forEach((comment) => {
+        createComments(comment, commentArea);
+      });
+      comments.textContent = `comments (${comm.length})`;
     }
   };
-  commentUpdate();
 
   const formContainer = document.createElement('div');
   formContainer.classList.add('form-container');
@@ -60,10 +62,8 @@ const Modal = async (dataname, dataimagemedium, dataid, datasummary) => {
   form.classList.add('form-comment');
   const name = document.createElement('input');
   name.classList.add('input-name');
-  name.placeholder = 'Enter you name...'
   const commentInput = document.createElement('textarea');
   commentInput.classList.add('text');
-  commentInput.placeholder = 'Write a message...'
   const commentBtn = document.createElement('button');
   commentBtn.classList.add('comment-btn');
   commentBtn.textContent = 'Submit';
@@ -71,16 +71,18 @@ const Modal = async (dataname, dataimagemedium, dataid, datasummary) => {
 
   commentBtn.addEventListener('click', async (e) => {
     e.preventDefault();
-    const details = await postComments(dataid, name.value, commentInput.value);
-    commentUpdate();
-    form.reset();
+    commentArea.innerHTML = '';
+    commentUpdate(commentArea);
+    name.value = '';
+    commentInput.value = '';
   });
+  commentUpdate(commentArea);
 
   const title = document.createElement('div');
   title.classList.add('popup-title-container');
   titlePopUp.classList.add('pop-title');
   title.append(titlePopUp, closeIcon);
-  commentsContainer.append(formContainer, commentsCard);
+  commentsContainer.append(commentsCard, formContainer);
   modalPopUp.append(title, imageSummary, comments, commentsContainer);
 
   Modal.style.display = 'flex';

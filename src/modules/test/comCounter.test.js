52e58d6271f00d41efe8e9f85bbd45d97fea3c06
Eternalgratis <tyaps002@gmail.com>
2022-08-06 14:23:49 +0100

    Fix linter

--- src/modules/test/comCounter.test.js
+++ src/modules/test/comCounter.test.js
@@ -1,26 +1,22 @@
-/* eslint-disable quotes */
-/* jshint camelcase: false */
-/* eslint-disable camelcase */
-// import api_mock from '../_mock_/api_mock.json';
-import comments from "./comments";
+import comments from './comments';
 
 const movieComments = [
   {
-    username: "Jhon",
-    comment: "Wowww...",
+    username: 'Jhon',
+    comment: 'Wowww...',
   },
   {
-    username: "Maria",
-    comment: "Cool",
+    username: 'Maria',
+    comment: 'Cool',
   },
   {
-    username: "Peter",
-    comment: "Nice movie",
+    username: 'Peter',
+    comment: 'Nice movie',
   },
 ];
 
-describe("Test Number of movies and comments", () => {
-  test("Test Number of comments", () => {
+describe('Test Number of movies and comments', () => {
+  test('Test Number of comments', () => {
     expect(comments(movieComments)).toBe(3);
   });
 });
